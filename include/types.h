69e435b95009b225bd32e2ae923d81de4955b1e5
nr <shibuyanorailgun@gmail.com>
2015-08-27 18:43:57 +0800

    fixed-size data type

--- include/types.h
+++ include/types.h
@@ -5,18 +5,24 @@
 #ifndef __NROS_TYPE_H
 #define __NROS_TYPE_H
 
+typedef unsigned char u8_t;
+
+typedef unsigned short u16_t;
+
+typedef unsigned int u32_t;
+
 typedef unsigned char uchar_t;
 
 typedef unsigned short ushort_t;
 
 typedef unsigned int uint_t;
 
-typedef uint_t pde_t;
-
-typedef uint_t pte_t;
-
 typedef enum bool_e {
 	false = 0, true = 1,
 } bool;
+
+typedef u32_t pde_t;
+
+typedef u32_t pte_t;
 
 #endif /* end of include guard: __NROS_TYPE_H */
